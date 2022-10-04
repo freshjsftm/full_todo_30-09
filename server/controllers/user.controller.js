b77bf2e3d0b0f77be7de56493e0d3f29e8875ab9
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-10-04 14:39:32 +0300

    Add image at server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,6 +5,9 @@ const {User} = require('../models');
 module.exports.createUser = async (req, res, next) => {
   try {
     const {body} = req;
+    if(req.file){
+      body.avatar = req.file.filename;
+    }
     const values = _.pick(body, ['login','password','avatar']);
     const user = await User.create(values);
     if(!user){
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const UserController = require('../controllers/user.controller')
+const UserController = require('../controllers/user.controller');
+const { upload } = require('../middleware/upload.mw');
 const userRouter = Router();
 
-userRouter.post('/', UserController.createUser)
+userRouter.post('/', upload ,UserController.createUser)
 //userRouter.get('/')
 //userRouter.get('/:userId')
 //userRouter.patch('/:userId')
